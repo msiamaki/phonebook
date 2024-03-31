f287929c16550aa689e36c2677163d48eaa23d89
MohammadSiamaki <msiamaki.4@gmail.com>
2024-03-31 15:14:44 +0330

    Added the Contact model admin panel and set it to be ordered by first-name, last-name then phone-number

--- phonebook_app/admin.py
+++ phonebook_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import register 
+
+from .models import Contact
 
 # Register your models here.
+
+
+@register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "phone_number")
+    list_display_links = ("first_name",)
+    search_fields = ("first_name", "last_name", "phone_number", "address")
--- phonebook_app/models.py
+++ phonebook_app/models.py
@@ -5,8 +5,8 @@ from django.core.validators import MinLengthValidator
 
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=50, null=False, verbose_name="First Name")
-    last_name = models.CharField(max_length=50, null=True, verbose_name="Last Name")
+    first_name = models.CharField(max_length=50, verbose_name="First Name", blank=False)
+    last_name = models.CharField(max_length=50, verbose_name="Last Name", blank=True)
     phone_number = models.CharField(
         max_length=11,
         validators=[MinLengthValidator(11)],
@@ -14,9 +14,13 @@ class Contact(models.Model):
         unique=True,
         verbose_name="Phone Number",
     )
-    address = models.TextField(null=True, verbose_name="Address")
-    description = models.TextField(null=True, verbose_name="Description")
+    address = models.TextField(blank=True, verbose_name="Address")
+    description = models.TextField(blank=True, verbose_name="Description")
 
     class Meta:
         verbose_name = "Contact"
         verbose_name_plural = "Contacts"
+        ordering = ("first_name", "last_name", "phone_number")
+
+    def __str__(self) -> str:
+        return self.first_name + " " + self.last_name
