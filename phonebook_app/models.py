689c6882cca5d5c5cabf939162f35464cf8d597d
MohammadSiamaki <msiamaki.4@gmail.com>
2024-03-31 13:51:51 +0330

    Modified fields of Contact model and made it better

--- phonebook_app/models.py
+++ phonebook_app/models.py
@@ -1,15 +1,22 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=50, min_length=1, null=False)
-    last_name = models.CharField(max_length=50, min_length=1, null=True)
-    phone_number = models.CharField(max_length=11, min_length=11, null=False, unique=True)
-    address = models.TextField(null=True)
-    description = models.TextField(null=True)
+    first_name = models.CharField(max_length=50, null=False, verbose_name="First Name")
+    last_name = models.CharField(max_length=50, null=True, verbose_name="Last Name")
+    phone_number = models.CharField(
+        max_length=11,
+        validators=[MinLengthValidator(11)],
+        null=False,
+        unique=True,
+        verbose_name="Phone Number",
+    )
+    address = models.TextField(null=True, verbose_name="Address")
+    description = models.TextField(null=True, verbose_name="Description")
 
     class Meta:
-        verbose_name = 'Contact'
-        verbose_name_plural = 'Contacts'
+        verbose_name = "Contact"
+        verbose_name_plural = "Contacts"
