74e2c60fdf3aa4723654f741be6b41122eef7ff7
MohammadSiamaki <msiamaki.4@gmail.com>
2024-03-31 13:42:33 +0330

    corrected the settings and added the Contact model

--- phonebook_app/models.py
+++ phonebook_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50, min_length=1, null=False)
+    last_name = models.CharField(max_length=50, min_length=1, null=True)
+    phone_number = models.CharField(max_length=11, min_length=11, null=False, unique=True)
+    address = models.TextField(null=True)
+    description = models.TextField(null=True)
+
+    class Meta:
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
