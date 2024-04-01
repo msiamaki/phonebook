1c4ccc472657a3bf50f3b7799398f720692da0de
MohammadSiamaki <msiamaki.4@gmail.com>
2024-04-01 13:08:51 +0330

    Completed the views part of the app

--- phonebook_app/views.py
+++ phonebook_app/views.py
@@ -1,3 +1,14 @@
+from rest_framework import viewsets, filters, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Contact
+from .serializers import ContactSerializer
 
 # Create your views here.
+
+
+class ContactViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all().order_by(["first_name", "last_name"])
+
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
